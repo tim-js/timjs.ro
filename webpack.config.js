9de6bd47d2c07304968590264c2fdac52aa6165c
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-10-17 21:06:08 +0300

    Add Webpack + React

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const APP_DIR = path.join(__dirname, 'src');
+const BUILD_DIR = path.join(__dirname, 'dist');
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    // multiple entry points
+    editions: APP_DIR + '/editions.js',
+  },
+  output: {
+    path: BUILD_DIR,
+    // [name] is placeholder for entry property name
+    // when dealing with multiple entry points
+    // will be replaced with it (eg. "editions")
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: APP_DIR
+    }]
+  }
+};
