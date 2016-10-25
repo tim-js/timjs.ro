d4828e30b2e8b05c4a62b66c8fd7cd86319e7d6b
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-10-25 18:30:44 +0300

    Highlight 3rd workshop as the active one

--- index.php
+++ index.php
@@ -12,13 +12,16 @@
             <ul class="workshops">
                 <li class="faded">
                     <img src="assets/javascript-citizens.svg" title="JavaScript Citizens workshop">
-                    <div><a href="http://www.meetup.com/tim-js/events/229574146/">23 Apr</a> && <a href="http://www.meetup.com/tim-js/events/230179755/">21 May</a></div>
+                    <div>
+                      <a href="http://www.meetup.com/tim-js/events/229574146/">23 Apr</a> &&
+                      <a href="http://www.meetup.com/tim-js/events/230179755/">21 May</a>
+                    </div>
                 </li>
-                <li>
+                <li class="faded">
                     <img src="assets/javascript-mechanics.svg" title="JavaScript Mechanics workshop">
                     <div><a href="http://www.meetup.com/tim-js/events/233420244/">22 Oct</a></div>
                 </li>
-                <li class="faded">
+                <li>
                     <img src="assets/javascript-neighborhoods.svg" title="JavaScript Neighborhoods workshop">
                     <div>// coming soon</div>
                 </li>
