728e66339d7b7fed841764ecd9fd1aae6ee6abe4
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-06 15:39:00 +0300

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,4 +1,4 @@
 <h1 class="logo"><img src="assets/tim.js-logo.svg" alt="tim.js logo" title="tim.js"></h1>
-<p class="grey">// timișoara javascript group</p>
+<p class="grey">// Timișoara JavaScript Community</p>
 
 <hr>
